83626e5728d62b2edef6f7b241268409e32473d9
MillaniaSameh <samehmillania@gmail.com>
2023-07-13 12:57:43 +0300

    Assignment 8 files added

--- Assignment 8 - Current time service with HTMX/CurrentTimeWithHTMX/Program.cs
+++ Assignment 8 - Current time service with HTMX/CurrentTimeWithHTMX/Program.cs
@@ -1,18 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.MapGet("/current-time", () =>
 {
-    Console.WriteLine("----------------I got here");
     var currentTime = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
     return Results.Ok(currentTime);
-});
-
-app.MapGet("/", async (HttpContext context) =>
-{
-    await context.Response.SendFileAsync("wwwroot/index.html");
 });
 
 app.Run();
