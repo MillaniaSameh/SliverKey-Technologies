17d5ecc02d4f1a4bd43cfd379062f8ae2d7b9ddd
MillaniaSameh <samehmillania@gmail.com>
2023-07-13 12:32:37 +0300

    Using async

--- Assignment 7 - Current time service/CurrentTimeService/Program.cs
+++ Assignment 7 - Current time service/CurrentTimeService/Program.cs
@@ -1,16 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.MapGet("/current-time", () =>
 {
     var currentTime = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
     return Results.Ok(currentTime);
-});
-
-app.MapGet("/", async (HttpContext context) => {
-    await context.Response.SendFileAsync("wwwroot/index.html");
 });
 
 app.Run();
