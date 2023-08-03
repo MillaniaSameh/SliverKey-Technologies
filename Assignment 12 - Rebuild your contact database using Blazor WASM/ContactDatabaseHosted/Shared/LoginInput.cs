cc8a454fcbd67f94a72b365ae6dff0b48aab26f2
MillaniaSameh <samehmillania@gmail.com>
2023-08-03 20:16:14 +0300

    Assignment 12 files added

---
+++ Assignment 12 - Rebuild your contact database using Blazor WASM/ContactDatabaseHosted/Shared/LoginInput.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ContactDatabaseHosted.Shared;
+
+public class LoginInput
+{
+    [JsonPropertyName("username")]
+    public string Username { get; set; } = "";
+
+    [JsonPropertyName("password")]
+    public string Password { get; set; } = "";
+}
